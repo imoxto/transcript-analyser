2d2dd72cd4767e3a348e3f17b14b7653dfb5f38e
abrarabedin001 <abrarabedin001@gmail.com>
2023-11-10 11:35:19 +0600

    added examples from lindow

--- index.ts
+++ index.ts
@@ -114,10 +114,51 @@ export async function analyzeTranscript(params: { transcript: string }) {
           Here is the summary for transcript 1: 
 
           The roofing salesperson reached out to Mr. Smith to inquire if the insurance adjuster had
-scheduled an inspection for the roofing project. Mr. Smith confirmed that the adjuster would be
-coming next Tuesday around 10:30 a.m. The salesperson offered to be present during the
-inspection to provide expertise, and they agreed to meet slightly before the adjuster's arrival.
-          `,
+          scheduled an inspection for the roofing project. Mr. Smith confirmed that the adjuster would be
+          coming next Tuesday around 10:30 a.m. The salesperson offered to be present during the
+          inspection to provide expertise, and they agreed to meet slightly before the adjuster's arrival.
+                    
+          Transcription 2
+
+          A: 	Good afternoon, class. Today, we'll delve into a niche yet incredibly impactful area of
+          marketing: targeting university students. This demographic, often aged between 18 to 24, is
+          unique, tech-savvy, and on the cusp of forming brand loyalties that can last a lifetime.
+          Understanding the demographic is crucial. University students are digital natives, having grown
+          up in the digital age. They're accustomed to online interactions, which makes them highly
+          receptive to digital marketing campaigns. With often limited budgets, these students are always
+          on the lookout for deals, discounts, and value propositions. Additionally, this generation is moresocially and environmentally aware than its predecessors. Brands that showcase sustainability
+          or social responsibility tend to resonate more with them.
+          When considering platforms to reach this audience, social media stands out. Platforms like
+          Instagram, TikTok, and Snapchat are particularly popular among this age group. Tailoring
+          content to these platforms can yield significant engagement. Another effective strategy is
+          influencer collaborations. Partnering with student influencers can offer brands an authentic
+          endorsement that their peers trust. Furthermore, direct collaborations with university clubs,
+          societies, or events can provide a direct and impactful access point to the student community.
+          One of the most important things to remember when marketing to this group is the importance
+          of authenticity. Students today can easily detect inauthentic marketing and are quick to call it
+          out. Brands need to be genuine in their messaging and ensure they uphold any promises made.
+          Instead of hard selling, brands should focus on creating engaging content. Content that
+          educates, entertains, or resonates on an emotional level tends to be more effective than overt
+          sales pitches.
+          Lastly, given the ever-evolving nature of the student demographic, it's essential for brands to
+          seek regular feedback and be adaptable. Methods like surveys or focus groups can be
+          invaluable in helping brands stay relevant and in tune with student needs and preferences.
+          A: 	In conclusion, marketing to university students requires a blend of authenticity,
+          understanding of their unique characteristics, and adaptability. Brands that can effectively
+          engage this demographic stand to not only gain loyal customers during their university years but
+          potentially for decades to come. Tomorrow, we'll look into some case studies of brands that
+          have successfully cracked this market. Until then, happy studying!
+          
+          Summary for transcript 2:
+          Marketing to University Students: Essentials
+          University students, aged 18-24, are digital natives keen on forming brand loyalties. They value
+          online engagement, seek deals, and resonate with socially responsible brands. Effective
+          engagement platforms include Instagram, TikTok, and Snapchat, with student influencer
+          collaborations offering genuine endorsements. Authenticity in brand messaging is crucial, with a
+          focus on content that educates or entertains. Continuous feedback, through tools like surveys,
+          ensures brands stay aligned with evolving student preferences.
+
+`,
           // description: "Summary of the transcript.",
         },
         keyPoints: {
@@ -159,6 +200,51 @@ inspection to provide expertise, and they agreed to meet slightly before the adj
           ● Prepare Documentation: Gather any relevant roofing project details, past assessments,
           or proposals to have on hand during the inspection.
           
+          Transcription 2
+
+          A: 	Good afternoon, class. Today, we'll delve into a niche yet incredibly impactful area of
+          marketing: targeting university students. This demographic, often aged between 18 to 24, is
+          unique, tech-savvy, and on the cusp of forming brand loyalties that can last a lifetime.
+          Understanding the demographic is crucial. University students are digital natives, having grown
+          up in the digital age. They're accustomed to online interactions, which makes them highly
+          receptive to digital marketing campaigns. With often limited budgets, these students are always
+          on the lookout for deals, discounts, and value propositions. Additionally, this generation is moresocially and environmentally aware than its predecessors. Brands that showcase sustainability
+          or social responsibility tend to resonate more with them.
+          When considering platforms to reach this audience, social media stands out. Platforms like
+          Instagram, TikTok, and Snapchat are particularly popular among this age group. Tailoring
+          content to these platforms can yield significant engagement. Another effective strategy is
+          influencer collaborations. Partnering with student influencers can offer brands an authentic
+          endorsement that their peers trust. Furthermore, direct collaborations with university clubs,
+          societies, or events can provide a direct and impactful access point to the student community.
+          One of the most important things to remember when marketing to this group is the importance
+          of authenticity. Students today can easily detect inauthentic marketing and are quick to call it
+          out. Brands need to be genuine in their messaging and ensure they uphold any promises made.
+          Instead of hard selling, brands should focus on creating engaging content. Content that
+          educates, entertains, or resonates on an emotional level tends to be more effective than overt
+          sales pitches.
+          Lastly, given the ever-evolving nature of the student demographic, it's essential for brands to
+          seek regular feedback and be adaptable. Methods like surveys or focus groups can be
+          invaluable in helping brands stay relevant and in tune with student needs and preferences.
+          A: 	In conclusion, marketing to university students requires a blend of authenticity,
+          understanding of their unique characteristics, and adaptability. Brands that can effectively
+          engage this demographic stand to not only gain loyal customers during their university years but
+          potentially for decades to come. Tomorrow, we'll look into some case studies of brands that
+          have successfully cracked this market. Until then, happy studying!
+          
+          Key Points:
+          ● Digital Natives: Students aged 18-24 are tech-savvy and forming brand loyalties.
+          ● Online Engagement: They prefer and respond well to digital interactions.
+          ● Value Seekers: Students are always on the lookout for deals and discounts.
+          ● Social Responsibility: They resonate with brands promoting sustainability and social
+          causes.
+          ● Top Platforms: Instagram, TikTok, and Snapchat are essential for engagement.
+          ● Influencer Collaborations: Partnering with student influencers provides genuine brand
+          endorsements.
+          ● Authentic Messaging: Brands must be genuine and avoid overt selling.
+          ● Content Strategy: Focus on content that educates, entertains, or emotionally connects.
+          ● Feedback is Key: Regular surveys or feedback mechanisms ensure alignment with
+          student preferences.
+
           `,
         },
         recommendations: {
@@ -202,7 +288,9 @@ inspection to provide expertise, and they agreed to meet slightly before the adj
              ● List Talking Points: Note key topics for the pre-inspection chat with Mr. Smith.
              ● Set Monday Reminder: Schedule a quick review for Monday evening to ensure
              readiness.
-             ● Check-in with Client: Send a brief text or email to Mr. Smith confirming Tuesday's plans.`,
+             ● Check-in with Client: Send a brief text or email to Mr. Smith confirming Tuesday's plans.
+             
+            `,
         },
       },
       required: ["summary", "keyPoints", "recommendations"],
@@ -256,24 +344,29 @@ async function main() {
   // B: I don't know.
   // A: Okay.
   // B: Bye.`;
-  const transcript = `A: There were, there was the infants' room. Next to that was the middle one, school, and then 
-                         you went up into the next one, which was, into the next room, which was divided by a 
-                         screen. That was all that it was. Just a screen across the… 
-                      B: What was the… 
-                      A: So if the headmaster shouted while we were in this class you could hear every word of what 
-                         he was saying. Anybody got the cane, you could hear it. 
-                      B: What was the dividing wall for, or screen for? 
-                      A: Well, it was so that if there was an assembly of any kind, it could be opened but it didn't 
-                         happen very often, only when the school was hired for the village, local village show or 
-                         something like that. We had one piano for those three classes and of course there were no 
-                         central heating then and we had a coal fire in the, with a guard round it. Then it eventually 
-                         got to a stove, and I think before I left school, the school, I think all that was done away with 
-                         and it was central heating then. But the other heat was from a big boiler at the side of the 
-                         school which my grandfather used to stoke morning and evening, and that kept the hot water 
-                         going. And on Friday nights my grandma had to clean the school thoroughly and we used to 
-                         go, my sister and I, and they were brass taps on the wash basins, and we used to go and 
-                         polish the brass taps for her and do all the wash basins and she used to give us sixpence each 
-                         for doing that. `
+  const transcript = `A: Hey sweetie! How's college life treating you?
+  B: Oh, Mom! It's been such a whirlwind. I've made some great friends, and my literature
+  class? Absolutely fascinating. We're diving into some classic novels I've never read before.
+  A: That sounds wonderful! I always knew you'd thrive in college. Have you been managing
+  to get enough sleep though? And eating well?
+  B: Mostly! Though, I admit, I've had a few too many late-night pizza runs with my
+  roommate, Jenna. She's from New York and insists she knows all the best pizza spots around.
+  A: (laughs) Well, as long as it's not every night. Speaking of food, have you thought about
+  when you're coming home for Thanksgiving? Your father and I are planning the dinner.
+  B: I've been looking at the train schedules. I think I can get a ticket for the Wednesday
+  afternoon before Thanksgiving. That way, I'll be home by evening.
+  A: Perfect! Oh, and since you've become quite the cook at college, how about you make one
+  of the dishes for dinner? Your grandmother always loved it when the family contributed.
+  B: Really? I'd love to! Jenna taught me this amazing vegan pumpkin pie recipe. It's a
+  twist on the classic, but I think everyone will love it.
+  A: Vegan pumpkin pie? Now that's something new for our table! I'm excited to try it. Just
+  send me the ingredients you'll need, and I'll have everything ready.
+  B: Will do, Mom. I can't wait to be home and see everyone. College is fun, but I've
+  missed you all.
+  A: We've missed you too, darling. But I'm glad you're enjoying yourself. Just remember to
+  study amidst all the fun, okay?
+  B: I promise, Mom. Talk to you soon. Love you!
+   `
 
   // this is exactly how the function will be called in the cloud function
   // Dont change the below lines
